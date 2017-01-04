1844aed564b8932d1ffba5cbb97ff4226a7038c3
Agastya Silvina <as362@st-andrews.ac.uk>
2017-01-04 00:30:48 +0000

    Updated picture

--- js/app.js
+++ js/app.js
@@ -7,16 +7,18 @@ var app = angular.module("app",  [
 
 app.controller('main', function($scope, $http, $sce) {
   $scope.htmlPopover = [
-    $sce.trustAsHtml("<img src='/img/art/art1.jpg' width='250'/>"),
+    $sce.trustAsHtml("<img src='/img/art/img0.jpg' width='250'/>"),
     $sce.trustAsHtml("<img src='/img/art/art2.jpg' width='250'/>"),
     $sce.trustAsHtml("<img src='/img/art/art3.jpg' width='250'/>"),
     $sce.trustAsHtml('<b style="color: blue">I love... </b>'),
-    $sce.trustAsHtml("<img src='/img/art/art5.jpg' width='250'/>"),
-    $sce.trustAsHtml("<img src='/img/art/art6.jpg' width='250'/><p class='text-right'><b> Hello there! Somebody I don't know</b></p>"),
+    $sce.trustAsHtml("<div><img src='/img/art/art5.jpg' width='250'/><img src='/img/art/art4.jpg' width='250'/></div>"),
+    $sce.trustAsHtml("<b style='color: black'>Contact</b>"),
     $sce.trustAsHtml("<img src='/img/art/its.jpg' width='250'/>"),
     $sce.trustAsHtml("<img src='/img/art/art8.png' width='250'/><p>If you <b>smile</b>, even if you're in a bad mood, it will <b>immediately improve</b> your mood...</p>"),
     $sce.trustAsHtml('<b style="color: blue">I love... </b><div class="label label-danger">Chocolate</div>'),
   ]
+
+  $scope.hideContact = false;
 
   var color = d3.scale.category20()
   $scope.options = {
